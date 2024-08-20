1a31b0e66e6ce01d1dbead9e10614081ce93e53b
EdinAyala <odayedin@gmail.com>
2024-08-19 19:46:43 -0600

    mi primer lab

---
+++ edin_ayala/src/app/catalogo/catalogo.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalogo.component.html',
+  styleUrl: './catalogo.component.css'
+})
+export class CatalogoComponent {
+
+  nombre_completo1: string = "Gerardo Anaya";
+  nombre_completo2: string= "Elias Enoc Echeverria";
+  nombre_completo3: string= "Tania Rosales";
+
+}
